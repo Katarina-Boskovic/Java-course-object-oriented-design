b91b6e31fa78f7a73abb45497b843df3acd147d4
Katarina Boskovic <katarina.boskovic@alphasights.com>
2019-10-02 12:30:14 +0100

    class and object

---
+++ src/Earth.java
@@ -0,0 +1,32 @@
+public class Earth {
+
+    // entry point for all java applications, code executed sequentially
+    public static void main(String args[]){
+
+        // define a variable (instance variable - points to the object) of type Human
+        Human tom;
+
+        // assign values to variable - object is created here
+        // new: used when calling the constructor method when creating a new object
+        tom = new Human();
+
+        // when constructor not explicitly defined need to assign values separately
+        tom.age = 5;
+        tom.eyeColour = "brown";
+        tom.heightInches = 72;
+        tom.name = "Tom Zsabo";
+
+        tom.speak();    // invoke speak method on the object
+        tom.walk();
+
+        Human joe = new Human();
+        joe.age = 22;
+        joe.eyeColour = "blue";
+        joe.heightInches = 69;
+        joe.name = "Jow Brown";
+
+        joe.speak();
+        joe.work();
+        joe.eat();
+    }
+}
---
+++ src/Human.java
@@ -0,0 +1,32 @@
+public class Human {
+
+    String name;
+    int age;
+    int heightInches;
+    String eyeColour;
+
+    // constructor method (same name as class) - do not have to define it explicitly
+    public Human() {
+
+    }
+
+    // methods
+    public void speak(){
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am " + heightInches + " tall");
+        System.out.println("I am " + age + " years old");
+        System.out.println("My eye colour is " + eyeColour);
+    }
+
+    public void eat(){
+        System.out.println("eating...");
+    }
+
+    public void walk(){
+        System.out.println("walking...");
+    }
+
+    public void work(){
+        System.out.println("working...");
+    }
+}
